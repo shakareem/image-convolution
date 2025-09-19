a8ba943d30b67d0797b35d480aeac61e0361c756
Karim Shakirov <karimshak06@gmail.com>
2025-09-19 22:31:52 +0300

    ci: build, test & lint
    Signed-off-by: Karim Shakirov <karimshak06@gmail.com>

--- app/src/test/kotlin/convolution/SerialConvolverTest.kt
+++ app/src/test/kotlin/convolution/SerialConvolverTest.kt
@@ -8,13 +8,12 @@ class SerialConvolverTest {
     private val smallHeight = 10
 
     @Test
-    fun `convolve white image with invert kernel becomes black`() {
+    fun `convolve white image with zero kernel becomes black`() {
         val image = Array(10) { DoubleArray(10) { 255.0 } }
 
-        val invertKernel = arrayOf(doubleArrayOf(0.0))
-        val inverted = SerialConvolver().convolve(image, invertKernel)
+        val blackenKernel = arrayOf(doubleArrayOf(0.0))
+        val blackened = SerialConvolver().convolve(image, blackenKernel)
 
-        val allBlack = inverted.all { row -> row.all { it == 0.0 } }
-        assertTrue(allBlack, "После фильтра все пиксели должны быть черные")
+        assertTrue(blackened.all { row -> row.all { it == 0.0 } })
     }
 }
